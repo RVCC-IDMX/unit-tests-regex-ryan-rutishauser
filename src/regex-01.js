bb1069adf6f140c5a52bfaa81556e5eed79bd9ab
ryan-rutishauser <98336166+ryan-rutishauser@users.noreply.github.com>
2022-10-08 21:04:20 -0400

    Make tests run

--- src/regex-01.js
+++ src/regex-01.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 /* eslint-disable prefer-regex-literals */
 /*
  * regex-01.js
@@ -18,7 +19,7 @@
  * ? example: testString('abc', /def/) // false
  */
 function testString(str, re) {
-  // write your code here & return value
+  return (re.test(str));
 }
 
 /**
@@ -32,64 +33,70 @@ function testString(str, re) {
  * ? must create a regular expression using the constructor function new RegExp()
  * ? https://mzl.la/3lWGpRa
  * ? hint: create a string with the join array method and the | character as the separator
- * ? The '|' character acts like the OR operator 
+ * ? The '|' character acts like the OR operator
  */
 function testStringOr(str, arr) {
-  // write your code here & return value
+  const tester = arr.join('|');
+  const regex = new RegExp(tester);
+  return regex.test(str);
 }
 
 /**
- * Tests the string for a match and ignore case.
- * @param {string} str - the string to test
- * @param {string} match -  the regular expression to be tested
- * @returns {boolean} - true if the string matches the regular expression,
- * false otherwise
- *
- * ? example: testStringIgnoreCase('ABC', 'a') // true
- * ? must create a regular expression using the constructor function new RegExp()
- */
+   * Tests the string for a match and ignore case.
+   * @param {string} str - the string to test
+   * @param {string} match -  the regular expression to be tested
+   * @returns {boolean} - true if the string matches the regular expression,
+   * false otherwise
+   *
+   * ? example: testStringIgnoreCase('ABC', 'a') // true
+   * ? must create a regular expression using the constructor function new RegExp()
+   */
 function testStringIgnoreCase(str, match) {
-  // write your code here & return value
+  const regex = new RegExp(match, 'i');
+  return (regex.test(str));
 }
 
 /**
- * Counts the number of times any vowel appears in the given string.
- * @param {string} str - the string to test
- * @returns {number} - the number of times any vowel appears in the string
- * ? Vowels aeiou can be upper or lower case
- * ? must use the match() string method - https://mzl.la/2Wfk0ps
- * ? example: countVowels('abcdef') => 2
- * ? example: countVowels('the boy is lost in the woods, HELP US!') => 10
- */
+   * Counts the number of times any vowel appears in the given string.
+   * @param {string} str - the string to test
+   * @returns {number} - the number of times any vowel appears in the string
+   * ? Vowels aeiou can be upper or lower case
+   * ? must use the match() string method - https://mzl.la/2Wfk0ps
+   * ? example: countVowels('abcdef') => 2
+   * ? example: countVowels('the boy is lost in the woods, HELP US!') => 10
+   */
 function countVowels(str) {
-  // write your code here & return value
+  const vowelCount = str.match(/[aeiou]/gi);
+  return vowelCount.length;
 }
 
 /**
- * Replaces all vowels with a passed character
- * @param {string} str - the string
- * @param {string} ch a character to replace vowels
- * @returns {string} - the string with vowels replaced by ch
- * ? example: replaceVowels('abcdef', '*') => '*bcd*f'
- * ? replace single vowels and vowel digraphs (like ee, ea, oo) with a single character
- * ? example: replaceVowels('the sound is sweet', '#') => 'th# s#nd #s sw#t'
- * ? must use the replace() string method - https://mzl.la/2Zsw4F6
- */
+   * Replaces all vowels with a passed character
+   * @param {string} str - the string
+   * @param {string} ch a character to replace vowels
+   * @returns {string} - the string with vowels replaced by ch
+   * ? example: replaceVowels('abcdef', '*') => '*bcd*f'
+   * ? replace single vowels and vowel digraphs (like ee, ea, oo) with a single character
+   * ? example: replaceVowels('the sound is sweet', '#') => 'th# s#nd #s sw#t'
+   * ? must use the replace() string method - https://mzl.la/2Zsw4F6
+   */
 function replaceVowels(str, ch) {
-  // write your code here & return value
+  const newString = str.replace(/[aeiou]+/gi, ch);
+  return newString;
 }
 
 /**
- * Tests (ignore case) the string for a given prefix that starts the string.
- * @param {string} str - the string to test for the prefix
- * @param {string} prefix - the prefix to test
- * @returns {boolean} - true if the string starts with the prefix, false otherwise
- * ? example: startsWith('bibliophile', 'biblio')  => true
- * ? example: startsWith('library', 'biblio')  => false
- * ? must create a regular expression using the constructor function new RegExp()
- */
+   * Tests (ignore case) the string for a given prefix that starts the string.
+   * @param {string} str - the string to test for the prefix
+   * @param {string} prefix - the prefix to test
+   * @returns {boolean} - true if the string starts with the prefix, false otherwise
+   * ? example: startsWith('bibliophile', 'biblio')  => true
+   * ? example: startsWith('library', 'biblio')  => false
+   * ? must create a regular expression using the constructor function new RegExp()
+   */
 function isPrefix(str, prefix) {
-  // write your code here & return value
+  const regex = new RegExp(`^${prefix}`, 'i');
+  return regex.test(str);
 }
 
 module.exports = {
